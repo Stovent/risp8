4140c77ba57c6818b376608d69e244e4e91e4e6c
Stovent <23307724+Stovent@users.noreply.github.com>
2025-05-28 21:14:38 -0400

    Update ratatui and crossterm to 0.29

--- risp8-tui/src/main.rs
+++ risp8-tui/src/main.rs
@@ -8,30 +8,32 @@ use risp8::{Chip8, ExecutionMethod, Receiver, Risp8Answer, Risp8Command, Screen,
 
 use ratatui::{Frame, Terminal, TerminalOptions, Viewport};
 use ratatui::backend::CrosstermBackend;
+use ratatui::buffer::Buffer;
+use ratatui::layout::{Constraint, Layout, Rect};
 use ratatui::style::Color;
-use ratatui::symbols::Marker;
-use ratatui::widgets::{Block, canvas::{Canvas, Rectangle}};
+use ratatui::text::Text;
+use ratatui::widgets::{Block, Widget};
 
 pub struct TuiApp {
-    marker: Marker,
-    screen: Screen,
     is_playing: bool,
     execution_method: ExecutionMethod,
+
+    screen_widget: ScreenWidget,
 }
 
 impl TuiApp {
     pub fn new() -> Self {
         Self {
-            marker: Marker::Block,
-            screen: DEFAULT_SCREEN,
             is_playing: false,
             execution_method: ExecutionMethod::Interpreter,
+
+            screen_widget: ScreenWidget::default(),
         }
     }
 
     pub fn run(&mut self, mut chip8: Chip8, chip8_in: Sender<Risp8Command>, chip8_out: Receiver<Risp8Answer>) -> std::io::Result<()> {
         if let Ok(size) = window_size() { // Not supported on Windows
-            if size.columns < risp8::State::SCREEN_WIDTH as u16 || size.rows < risp8::State::SCREEN_HEIGHT as u16 {
+            if size.columns < State::SCREEN_WIDTH as u16 || size.rows < State::SCREEN_HEIGHT as u16 {
                 println!("Warning: terminal is smaller than Chip8 screen");
             }
         }
@@ -43,7 +45,7 @@ impl TuiApp {
         let terminal_options = TerminalOptions {
             viewport: Viewport::Fullscreen,
         };
-        let mut terminal = Terminal::with_options(backend, terminal_options).unwrap();
+        let mut terminal = Terminal::with_options(backend, terminal_options)?;
         terminal.clear()?;
 
         std::thread::spawn(move || {
@@ -53,14 +55,14 @@ impl TuiApp {
         loop {
             while let Ok(Some(answer)) = chip8_out.try_recv() {
                 match answer {
-                    Risp8Answer::Screen(s) => self.screen = s,
+                    Risp8Answer::Screen(s) => self.screen_widget.screen = s,
                     Risp8Answer::PlaySound => (),
                     Risp8Answer::StopSound => (),
                 }
             }
             chip8_in.send(Risp8Command::GetScreen).unwrap();
 
-            terminal.draw(|frame| self.ui(frame)).unwrap();
+            terminal.draw(|frame| self.ui(frame))?;
 
             if self.handle_keyboard(&chip8_in)? { // Exit requested
                 break;
@@ -73,53 +75,38 @@ impl TuiApp {
     }
 
     fn ui(&self, frame: &mut Frame) {
+        use Constraint::{Length, Min};
+
+        let frame_area = frame.area();
+        let [title_area, screen_block_area] = Layout::vertical([Length(1), Min(0)]).areas(frame_area);
+        let screen_block = Block::bordered();
+        let screen_area = screen_block.inner(screen_block_area);
+
+        let screen_title = self.get_title(screen_area);
+        let screen_block = screen_block.title(screen_title);
+
+        let frame_title = format!("<q> Quit | <p> Play | <iklmj> Execution | {}x{}", frame_area.width, frame_area.height);
+        let frame_title = if frame_area.width > frame_title.len() as u16 { // Always show the important information.
+            Text::from(frame_title).centered()
+        } else {
+            Text::from(frame_title).left_aligned()
+        };
+
+        frame.render_widget(frame_title, title_area);
+        frame.render_widget(screen_block, screen_block_area);
+        self.screen_widget.render(screen_area, frame.buffer_mut());
+    }
+
+    fn get_title(&self, screen_area: Rect) -> String {
         let playing = if self.is_playing { "Running" } else { "Paused" };
         let exec = match self.execution_method {
             ExecutionMethod::Interpreter => "Interpreter",
-            ExecutionMethod::CachedInterpreter => "Cached Interpreter",
+            ExecutionMethod::CachedInterpreter => "Cached Interpreter 1",
             ExecutionMethod::CachedInterpreter2 => "Cached Interpreter 2",
             ExecutionMethod::CachedInterpreter3 => "Cached Interpreter 3",
             ExecutionMethod::Jit => "JIT",
         };
-        let screen_title = format!("{playing} | {exec}");
-
-        let screen_block = Block::bordered()
-            .title(screen_title);
-
-        let canvas: Canvas<'_, _> = Canvas::default()
-            .block(screen_block)
-            .x_bounds([0.0, State::SCREEN_WIDTH as f64])
-            .y_bounds([0.0, State::SCREEN_HEIGHT as f64])
-            .marker(self.marker)
-            .paint(|ctx| {
-                for y in 0..State::SCREEN_HEIGHT {
-                    for x in 0..State::SCREEN_WIDTH {
-                        let color = if self.screen[y][x] { Color::White } else { Color::Black };
-
-                        let rect = Rectangle {
-                            x: x as f64,
-                            y: (State::SCREEN_HEIGHT - 1 - y) as f64,
-                            width: 1.0,
-                            height: 1.0,
-                            color,
-                        };
-                        ctx.draw(&rect);
-                    }
-                }
-            });
-
-        let size = frame.size();
-        frame.render_widget(canvas, size);
-    }
-
-    fn change_marker(&mut self) {
-        self.marker = match self.marker {
-            Marker::Dot => Marker::Braille,
-            Marker::Braille => Marker::Block,
-            Marker::Block => Marker::HalfBlock,
-            Marker::HalfBlock => Marker::Bar,
-            Marker::Bar => Marker::Dot,
-        };
+        format!("{playing} | {exec} | {}x{}", screen_area.width, screen_area.height)
     }
 
     /// Returns `Ok(true)` when exit is requested.
@@ -183,7 +170,6 @@ impl TuiApp {
                                 chip8_in.send(Risp8Command::SetExecutionMethod(ExecutionMethod::Jit)).unwrap();
                                 self.execution_method = ExecutionMethod::Jit;
                             },
-                            Char('a') => self.change_marker(),
                             _ => (),
                         }
                     }
@@ -192,6 +178,44 @@ impl TuiApp {
         }
 
         Ok(false)
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct ScreenWidget {
+    screen: Screen,
+}
+
+impl Default for ScreenWidget {
+    fn default() -> Self {
+        Self {
+            screen: DEFAULT_SCREEN,
+        }
+    }
+}
+
+impl Widget for &ScreenWidget {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        if area.is_empty() {
+            return;
+        }
+
+        let width_ratio = State::SCREEN_WIDTH as f32 / area.width as f32;
+        let height_ratio = State::SCREEN_HEIGHT as f32 / area.height as f32;
+
+        for y in 0..area.height {
+            for x in 0..area.width {
+                let yy = (y as f32 * height_ratio) as usize;
+                let xx = (x as f32 * width_ratio) as usize;
+                let color = if self.screen[yy][xx] { Color::White } else { Color::Black };
+
+                let pos = (area.x + x, area.y + y);
+                buf[pos].set_fg(color).set_bg(color);
+            }
+        }
     }
 }
 
