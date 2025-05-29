a9f62edc1ac9c8e59ad0bb6e9a4c04ab865cc8fc
Stovent <23307724+Stovent@users.noreply.github.com>
2025-05-28 21:14:35 -0400

    Implement cache invalidation to support self-modifying code

--- src/cache.rs
+++ src/cache.rs
@@ -2,53 +2,49 @@ use dynasmrt::{dynasm, DynasmApi, x64::Assembler, AssemblyOffset, mmap::Executab
 
 pub struct Cache {
     pub pc: u16,
+    /// The address of the instruction following the last instruction in this cache [pc, end_pc).
+    end_pc: u16,
     _called_buf: ExecutableBuffer, // Store it so its memory isn't freed.
-    caller_buf: ExecutableBuffer, // Store it so its memory isn't freed.
-    caller: extern "win64" fn(&mut u32),
+    _caller_buf: ExecutableBuffer, // Store it so its memory isn't freed.
+    caller: extern "win64" fn(&mut u64),
 }
 
 impl Cache {
-    pub fn new(pc: u16, called_buf: ExecutableBuffer) -> Self {
-        let mut caller_buf = Assembler::new().expect("Failed to create new assembler");
+    pub fn new(pc: u16, end_pc: u16, called_buf: ExecutableBuffer) -> Self {
+        let mut caller_asm = Assembler::new().expect("Failed to create new assembler");
         let called = called_buf.ptr(AssemblyOffset(0));
 
-        extern "win64" fn dummy(_: &mut u32) {}
-        let mut cache = Self {
-            pc,
-            _called_buf: called_buf,
-            caller_buf: Assembler::new().expect("Dummy assembler").finalize().unwrap(),
-            caller: dummy,
-        };
+        #[cfg(debug_assertions)] println!("New cache at {pc:#X} (end {end_pc:#X}, {called:?})");
 
-        #[cfg(debug_assertions)] println!("New cache at {:#X} (size {}, {:?})", pc, cache._called_buf.size(), called);
-
-        // Saves the caller-saved registers RAX, RCX and RDX.
-        // Call the cached code.
-        // Move the return value to the `ret` variable.
-        // Restore the registers and lend control back to the function.
-        dynasm!(caller_buf
+        dynasm!(caller_asm
             ; .arch x64
-            ; push rax
+            ; push rax // Saves the caller-saved registers RAX, RCX and RDX.
             ; push rcx
             ; push rdx
-            ; mov rax, QWORD called as i64
+            ; mov rax, QWORD called as i64 // Call the cached code.
             ; call rax
-            ; pop rdx
+            ; pop rdx// Restore the registers and lend control back to the function.
             ; pop rcx
-            ; mov DWORD [rcx], eax
+            ; mov QWORD [rcx], rax // Move the return value to the `ret` variable.
             ; pop rax
             ; ret
         );
 
-        cache.caller_buf = caller_buf.finalize().unwrap();
-        unsafe {
-            cache.caller = std::mem::transmute::<*const u8, extern "win64" fn(&mut u32)>(cache.caller_buf.ptr(AssemblyOffset(0)))
-        }
+        let _caller_buf = caller_asm.finalize().unwrap();
+        let caller = unsafe {
+            std::mem::transmute::<*const u8, extern "win64" fn(&mut u64)>(_caller_buf.ptr(AssemblyOffset(0)))
+        };
 
-        cache
+        Self {
+            pc,
+            end_pc,
+            _called_buf: called_buf,
+            _caller_buf,
+            caller,
+        }
     }
 
-    pub fn run(&mut self) -> u32 {
+    pub fn run(&self) -> u64 {
         #[cfg(debug_assertions)] println!("Executing cache at {:#X}", self.pc);
         let mut ret = 0;
         (self.caller)(&mut ret);
@@ -68,11 +64,23 @@ impl Caches {
         }
     }
 
-    pub fn add(&mut self, pc: u16, exec: ExecutableBuffer) {
-        self.caches.push(Cache::new(pc, exec));
+    pub fn add(&mut self, pc: u16, end_pc: u16, exec: ExecutableBuffer) {
+        self.caches.push(Cache::new(pc, end_pc, exec));
     }
 
-    pub fn get(&mut self, pc: u16) -> Option<&mut Cache> {
-        self.caches.iter_mut().find(|el| el.pc == pc)
+    pub fn get(&self, pc: u16) -> Option<&Cache> {
+        self.caches.iter().find(|cache| cache.pc == pc)
+    }
+
+    pub fn clear(&mut self) {
+        self.caches.clear();
+    }
+
+    /// Deletes all the caches that contain the given address range. end_addr inclusive.
+    pub fn invalidate(&mut self, beg_addr: u16, end_addr: u16) {
+        let _: Vec<_> = self.caches.drain_filter(|cache| {
+            beg_addr >= cache.pc && beg_addr < cache.end_pc ||
+            end_addr >= cache.pc && end_addr < cache.end_pc
+        }).collect();
     }
 }
